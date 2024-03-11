6d0481ba97e3d3e00aa28e1fac98cb98e49da3f3
LocalReach <thelocalreach@gmail.com>
2024-03-10 23:56:18 -0400

    penis2

--- livestream.py
+++ livestream.py
@@ -3,8 +3,8 @@ import time
 
 # Initialize the video capture from the first camera device
 cap = cv2.VideoCapture(0)
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 cap.set(cv2.CAP_PROP_FPS, 60)
 
 # Check if the camera opened successfully
